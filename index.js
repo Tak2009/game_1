5586af864da2ee885e807666606416f06704f522
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-03-11 20:35:21 +0000

    second lesson done

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-let screen = document.querySelector("#screen");
-let context = screen.getContext("2d");
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+
+// ブロックの縦横：正方形の一辺
 const blockSize = 30;
+// テトロミノのサイズ
 const tetroSize = 4;
+// フィールドサイズ by Block
+const fieldCol = 10; //横に１０個
+const fieldRow = 20; //縦に２０個
+// スクリーンサイズ
+const screenWitdth = blockSize * fieldCol
+const screenHeight = blockSize * fieldRow
+
+canvas.width = screenWitdth;
+canvas.height = screenHeight;
+canvas.style.border = "4px solid #555"
+
 // context.fillStyle = "red";
 // context.fillRect(0,0,blockSize,blockSize);
+
+// テトロミノの本体
 let tetro = [
-    [0,0,0,0],
+    [0,0,0,0], //z
     [1,1,0,0],
     [0,1,1,0],
     [0,0,0,0]
@@ -14,9 +30,11 @@ let tetro = [
 let tetroX = 0;
 let tetroY = 0;
 
-
 // テトロミノを表示する関数
 const drawTetro = () => {
+
+// 一旦消す
+    context.clearRect(0,0,screenWitdth, screenHeight);
 //y 縦軸: 各配列　x 横軸: 各配列の中
     for (let y = 0; y < tetroSize; y++){
         for (let x = 0; x < tetroSize; x++){
@@ -32,27 +50,29 @@ const drawTetro = () => {
     }
 }
 
-document.onkeydown = (e) => {
-    // onkeydown keyCode検索
+drawTetro();
 
-    switch(e.keyCode){
-        case 37: //左
+document.onkeydown = (e) => {
+    // Code検索
+　console.log(e.code)
+    switch(e.code){
+        case "ArrowLeft": //左
         tetroX--;
         break;
-        case 38: //上
+        case "ArrowUp": //上
         tetroY--;
         break;
-        case 39: //右
+        case "ArrowRight": //右
         tetroX++;
         break;
-        case 40: //下
+        case "ArrowDown": //下
         tetroY++;
         break;
-        case 32: //スペース
+        case "Space": //スペース
         break;
     }
 
     drawTetro();
 };
 
-drawTetro();
+
