4f5930da0102829784c699139bc779eefc226167
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-03-14 10:31:10 +0000

    #7 completed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let can = document.querySelector("#canvas");
 let ctx = can.getContext("2d");
-
 // a square side length
 const blockSize = 30;
 // tetromino size
@@ -11,22 +10,78 @@ const fieldRow = 20; // 20 rows in height
 // playing screen size in pixcel
 const screenWitdth = blockSize * fieldCol
 const screenHeight = blockSize * fieldRow
-let speed = 300;
 
 can.width = screenWitdth;
 can.height = screenHeight;
 can.style.border = "4px solid #555"
 
-// tetromino patterns
-let tetro = [
-    [0,0,0,0], //z
-    [1,1,0,0],
-    [0,1,1,0],
-    [0,0,0,0]
-];
+let speed = 300;
+let gameOver = false;
+
+let colorList = [
+    "#000", // for the empty array in tetroList
+    "#FD2",
+    "#66F",
+    "#6CF",
+    "#F44",
+    "#6B6",
+    "#FA2",
+    "#C5C"
+]
+
+let tetroList = [
+    [], // this is to avoid "0" in tetroType in fixTetro()
+    [
+        [0,0,0,0], //i
+        [1,1,1,1],
+        [0,0,0,0],
+        [0,0,0,0]
+    ],
+    [
+        [0,1,0,0], //l
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,0,0]
+    ],
+    [
+        [0,0,1,0], //j
+        [0,0,1,0],
+        [0,1,1,0],
+        [0,0,0,0]
+    ],
+    [
+        [0,1,0,0], //t
+        [0,1,1,0],
+        [0,1,0,0],
+        [0,0,0,0]
+    ],
+    [
+        [0,0,0,0], //o
+        [0,1,1,0],
+        [0,1,1,0],
+        [0,0,0,0]
+    ],    
+    [
+        [0,0,0,0], //z
+        [1,1,0,0],
+        [0,1,1,0],
+        [0,0,0,0]
+    ],
+    [
+        [0,0,0,0], //s
+        [0,1,1,0],
+        [1,1,0,0],
+        [0,0,0,0]
+    ]
+]
+
+let tetroType = Math.floor(Math.random() * (tetroList.length - 1)) + 1
+
+// tetromino
+let tetro = tetroList[tetroType]
+const startX = fieldCol/2 -tetroSize/2;
 
 let field = [];
-
 
 // initializing a field 
 const initializeField = () => {
@@ -40,16 +95,27 @@ const initializeField = () => {
     }
 }
 
-const drawOneBlock = (x, y) => {
+const drawOneBlock = (x, y, c) => {
     let px = x * blockSize;
     let py = y * blockSize;
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = colorList[c];
     ctx.fillRect(px,py,blockSize,blockSize);
     ctx.strokeStyle="black";
     ctx.strokeRect(px,py,blockSize,blockSize);
 
 }
 
+
+const gameOverMessage = (message) => {
+    ctx.font = "40px 'MS Gothic'";
+    let w = ctx.measureText(message).width;
+    let x = screenWitdth/2 - w/2;
+    let y = screenHeight/2 - 20;
+    ctx.lineWidth = 8;
+    ctx.strokeText(message, x, y);
+    ctx.fillStyle = "white";
+    ctx.fillText(message, x, y);
+}
 // display field
 const drawField = () => {
         ctx.clearRect(0,0,screenWitdth, screenHeight);
@@ -57,14 +123,14 @@ const drawField = () => {
         for (let y = 0; y < fieldRow; y++){
             for (let x = 0; x < fieldCol; x++){
                 if(field[y][x]){
-                    drawOneBlock(x, y)
+                    drawOneBlock(x, y, field[y][x])
                 }
             }
         }
     }
 
 // tetromino coordinate 
-let tetroX = 0;
+let tetroX = startX;
 let tetroY = 0;
 
 // display tetromino
@@ -72,10 +138,13 @@ const drawTetro = () => {
     for (let y = 0; y < tetroSize; y++){
         for (let x = 0; x < tetroSize; x++){
             if(tetro[y][x]){
-                drawOneBlock(tetroX + x, tetroY + y)
+                drawOneBlock(tetroX + x, tetroY + y, tetroType)
             }
         }
     }
+    if (gameOver){
+        gameOverMessage("Game Over")
+    } 
 }
 
 const checkMovement = (mx, my, newTetro = tetro) => {
@@ -116,19 +185,61 @@ const fixTetro = () => {
     for (let y = 0; y < tetroSize; y++){
         for (let x = 0; x < tetroSize; x++){
             if(tetro[y][x]){
-                field[tetroY + y][tetroX + x] = 1;
+                //　if tetroList does not have an empty array in it at index 0, i shape always disappear
+                field[tetroY + y][tetroX + x] = tetroType;
+            }
+        }
+    }
+}
+
+// line check
+const checkLine = () =>{
+    let lineCount = 0;
+    for (let y = 0; y < fieldRow; y++){
+        let checkFlag = true;
+        for (let x = 0; x < fieldCol; x++){
+            if (!field[y][x]){
+                checkFlag = false;
+                break;
+            }
+        }
+        // if a line is filled with blocks
+        if (checkFlag){
+            lineCount++;
+            for (let ny = y; ny > 0; ny--){
+                for (let nx = 0; nx < fieldCol; nx++){
+                field[ny][nx] = field[ny - 1][nx];
+                }
             }
         }
     }
 }
 
 const dropTetro = () => {
+    // if gameover = true, no more tetro comes up from the top
+    if(gameOver){
+        return;
+    }
+
     if (checkMovement(0, 1)){
         tetroY++
     } else {
+        // anchor the tetro once it hits the bottom or a block
         fixTetro();
-        tetroX = 0;
+        // check the line = all filled 
+        checkLine();
+        // tetro type needs resetting for a next tetro
+        tetroType = Math.floor(Math.random() * (tetroList.length - 1)) + 1
+        tetro = tetroList[tetroType]
+        // reset the coordinate for a new tetro
+        tetroX = startX;
         tetroY = 0;
+
+        // game over check
+        if(!checkMovement(0, 0)){
+            gameOver = true;
+        }
+
     }
 
     drawField();
@@ -145,7 +256,10 @@ drawTetro();
 setInterval(dropTetro, speed)
 
 document.onkeydown = (e) => {
-    // check key movement 
+    // if gameover = true, no key entry is accepted 
+    if(gameOver){
+        return;
+    }
 　console.log(e.code)
     switch(e.code){
         case "ArrowLeft": //左
